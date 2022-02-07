22728b081216a559da59d66d0d91146819a60535
Guilherme Scarpel <guilherme@portao3.com.br>
2022-02-07 13:22:31 -0300

    added scoped styles

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import { BrowserRouter } from 'react-router-dom';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default function App() {
   return (
-    <div className="app">
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div className="app">
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 }
