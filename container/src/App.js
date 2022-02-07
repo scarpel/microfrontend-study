f4f694fc07c598ac9f339e9a7663dbec09fe6782
Guilherme Scarpel <guilherme@portao3.com.br>
2022-02-07 12:07:15 -0300

    some changes

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App() {
   return (
     <div className="app">
-      <h1>Container</h1>
+      <h1>Container App</h1>
       <hr />
       <MarketingApp />
     </div>
