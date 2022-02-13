dd6f6e4e6da775a40e0823514f323e6332b9b75d
Guilherme Scarpel <guilherme@portao3.com.br>
2022-02-13 09:48:01 -0300

    added dashboard

--- dashboard/src/bootstrap.js
+++ dashboard/src/bootstrap.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue';
 import Dashboard from './components/Dashboard.vue';
 
 const mount = (el) => {
-  if (el) {
-    const app = createApp(Dashboard);
-    app.mount(el);
-  }
+  const app = createApp(Dashboard);
+  app.mount(el);
 };
 
 if (process.env.NODE_ENV === 'development') {
